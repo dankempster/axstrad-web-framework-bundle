0d8996764e4b3da262b6247ba1221185432a7853
Dan Kempster <dev@dankempster.co.uk>
2014-12-30 15:52:38 +0000

    Update doc blocks with copyright, license, author, package and subpackage (where appropriate) annotations.

--- AxstradWebFrameworkBundle.php
+++ AxstradWebFrameworkBundle.php
@@ -1,9 +1,26 @@
 <?php
+/**
+ * This file is part of the Axstrad library.
+ *
+ * (c) Dan Kempster <dev@dankempster.co.uk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright 2014-2015 Dan Kempster <dev@dankempster.co.uk>
+ */
 
 namespace Axstrad\WebFrameworkBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Axstrad\WebFrameworkBundle\AxstradWebFrameworkBundle
+ *
+ * @author Dan Kempster <dev@dankempster.co.uk>
+ * @license MIT
+ * @package Axstrad/WebFrameworkBundle
+ */
 class AxstradWebFrameworkBundle extends Bundle
 {
 }
